5cd8f761ba6a24976aa2e776b36aac63d40b5fb4
Kenzie Gibson <gibsonkenzie76@gmail.com>
2025-04-27 14:18:24 +0100

    Created script to edit CharacterBody2d node

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,11 +1,8 @@
-extends CharacterBody2D
+extends CharacterBody2D # this is the node that the script will extend
 
+var speed = 400 # speed of the ping pong ball
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+## GDscript has two types of functions, regular like Python where functions are created and called,
+## inbuilt functions where the function is created but Godot will call it for you at specific times,
+## e.g ready function - executed when a scene is ready, _process if executed at every frame but not 
+## designed for physics interactions, _physics_process is the same but used for physics interactions
