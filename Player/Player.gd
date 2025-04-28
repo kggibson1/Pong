e79486d19d35bf13a1a8daf3e9832a4568393280
Kenzie Gibson <gibsonkenzie76@gmail.com>
2025-04-28 09:26:09 +0100

    Pladdle moves up and down with up an down keys, upper boundry addded to game to avoid pladdle leaving the screen

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D # this is the node that the script will extend
 
-var speed = 400 # speed of the ping pong ball
+var speed = 5 # speed of the ping pong ball
 
 ## GDscript has two types of functions, regular like Python where functions are created and called,
 ## inbuilt functions where the function is created but Godot will call it for you at specific times,
@@ -27,7 +27,7 @@ func _physics_process(_delta: float):
 	velocity = velocity.normalized()
 		
 	# now that velocity is adjusted we move the player (paddle) with the new velocity
-	move_and_collide(velocity)
+	move_and_collide(velocity*speed) # not sure why move_and_slide doesnt work - need to look into that
 	
 	
 	
