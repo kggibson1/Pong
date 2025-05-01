1b3a926ab9db6944262c51f442936460cb263304
Kenzie Gibson <gibsonkenzie76@gmail.com>
2025-05-01 20:04:14 +0100

    _ready function added for ball - randomise starting direction of ball - more to be added

--- Ball/ball.gd
+++ Ball/ball.gd
@@ -1,3 +1,8 @@
 extends CharacterBody2D # add to the ball scene
 
 var speed = 600 # speed of the ball
+#var velocity = Vector2.ZERO # vector that doesnt point in any direction, dont need to set as the CharacterBody2D already has this feature, it is editied with the move_and_slide function
+
+func _ready():
+	# only going to run once when the ball scene is ready
+	velocity.x = [-1,1][randi() % 2]
