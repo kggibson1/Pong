e743fdbe4228639d6540f6fa11e30476e23beee4
Kenzie Gibson <gibsonkenzie76@gmail.com>
2025-05-12 18:28:09 +0100

    Added randomize function to generate new seed upon loading of a new game

--- Ball/ball.gd
+++ Ball/ball.gd
@@ -3,11 +3,17 @@ extends CharacterBody2D # add to the ball scene
 var speed = 600 # speed of the ball
 #var velocity = Vector2.ZERO # vector that doesnt point in any direction, dont need to set as the CharacterBody2D already has this feature, it is editied with the move_and_slide function
 
-## only going to run once when the ball scene is ready
+## only going to run once when the ball scene is ready. Underline means that functions are not directly called
+## by a user, but called at specific points in a game process by godot.
 func _ready():
+	# have a new seed generated for each new scene ready (not sure if this is needed in the newer versions)
+	randomize()
 	
 	# randomising the velocity of the ball
-	velocity.x = [-1,1][randi() % 2] # give a random number between -1 and 1, [randi() % 2] generates random number between 0 and 1, randi() generates int over infinite range, % 2 limits this to 0 and 1
+	velocity.x = [-1,1][randi() % 2]*speed # give a random number between -1 and 1, [randi() % 2] generates random number between 0 and 1, randi() generates int over infinite range, % 2 limits this to 0 and 1
 	
 	# make ball move faster in the horizontal direction than the vertical direction
-	velocity.y = [-0.8, 0.8][randi() % 2]
+	velocity.y = [-0.8, 0.8][randi() % 2]*speed
+
+func _physics_process(delta):
+	move_and_slide()
